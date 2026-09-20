d637bd09c8326dc917ec476cb8a6126e726a2b49
jedick <j3ffdick@gmail.com>
2026-09-20 16:20:47 +0800

    Add Chinese train/test data

--- model/data/push_dataset_to_hub.py
+++ model/data/push_dataset_to_hub.py
@@ -4,9 +4,12 @@
 from datasets import Dataset, DatasetDict
 from huggingface_hub import login, whoami
 
+# Set the language here
+lang = "en"
+
 # Build the dataset from these CSV files
-train_csv_path = "langcalc_train_data.csv"
-test_csv_path = "langcalc_test_data.csv"
+train_csv_path = f"langcalc_train_data_{lang}.csv"
+test_csv_path = f"langcalc_test_data_{lang}.csv"
 
 dataset = DatasetDict(
     {
@@ -23,7 +26,7 @@ login()
 
 # Construct the HF repo_id for the dataset
 username = whoami()["name"]
-repo_id = f"{username}/langcalc-data"
+repo_id = f"{username}/langcalc-{lang}"
 
-dataset.push_to_hub(repo_id)
+dataset.push_to_hub(repo_id, private=False)
 print(f"Pushed dataset to https://huggingface.co/datasets/{repo_id}")
